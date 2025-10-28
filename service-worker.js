2d960fd857ce0093af5b66aed572425be18c3972
Tanushsatgur <tanushk599@gmail.com>
2025-10-28 19:57:41 +0500

    Fix syntax for event listeners in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('flappyskull-cache').then(cache => {
-      return cache.addAll(['/', '/index.html']);
+    caches.open("flappyskull-cache").then(cache => {
+      return cache.addAll(["/", "/index.html"]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
